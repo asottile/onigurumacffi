8e9c5a416ed31f7659d2c674fb2d0663ff9e2823
Anthony Sottile <asottile@umich.edu>
2020-02-10 21:57:23 -0800

    Build abi3 wheels

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+import platform
+import sys
+
 from setuptools import setup
 
-setup(cffi_modules=['onigurumacffi_build.py:ffibuilder'])
+if sys.platform != 'win32' and platform.python_implementation() == 'CPython':
+    try:
+        import wheel.bdist_wheel
+    except ImportError:
+        cmdclass = {}
+    else:
+        class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
+            def finalize_options(self) -> None:
+                self.py_limited_api = f'cp3{sys.version_info[1]}'
+                super().finalize_options()
+
+        cmdclass = {'bdist_wheel': bdist_wheel}
+else:
+    cmdclass = {}
+
+setup(cffi_modules=['onigurumacffi_build.py:ffibuilder'], cmdclass=cmdclass)
