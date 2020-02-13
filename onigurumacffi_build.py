55623b6041f193d17659b96f0e2417f6edf0c6b7
Anthony Sottile <asottile@umich.edu>
2020-02-12 18:47:19 -0800

    simplify the types

--- onigurumacffi_build.py
+++ onigurumacffi_build.py
@@ -12,45 +12,30 @@ typedef unsigned char OnigUChar;
 
 struct re_pattern_buffer;
 typedef struct re_pattern_buffer OnigRegexType;
-typedef OnigRegexType* OnigRegex;
 typedef OnigRegexType regex_t;
 
-struct OnigEncodingTypeST{};
+struct OnigEncodingTypeST{...;};
 typedef struct OnigEncodingTypeST OnigEncodingType;
-typedef OnigEncodingType* OnigEncoding;
 extern OnigEncodingType OnigEncodingUTF8;
 
-typedef struct {} OnigSyntaxType;
+typedef struct {...;} OnigSyntaxType;
 extern OnigSyntaxType OnigSyntaxOniguruma;
 
-typedef struct {
-    OnigEncoding enc;
-    OnigUChar* par;
-    OnigUChar* par_end;
-} OnigErrorInfo;
-
-typedef struct OnigCaptureTreeNodeStruct {
-    int group;
-    int beg;
-    int end;
-    int allocated;
-    int num_childs;
-    struct OnigCaptureTreeNodeStruct** childs;
-} OnigCaptureTreeNode;
+typedef struct {...;} OnigErrorInfo;
 
 struct re_registers {
     int allocated;
     int num_regs;
     int* beg;
     int* end;
-    OnigCaptureTreeNode* history_root;
+    ...;
 };
 typedef struct re_registers OnigRegion;
 
 OnigRegion* onig_region_new(void);
 void onig_region_free(OnigRegion* region, int free_self);
 
-int onig_initialize(OnigEncoding use_encodings[], int num_encodings);
+int onig_initialize(OnigEncodingType* use_encodings[], int num_encodings);
 
 int onig_error_code_to_str(OnigUChar* s, int err_code, ...);
 
@@ -58,12 +43,12 @@ int onig_new(
     regex_t** reg,
     const OnigUChar* pattern, const OnigUChar* pattern_end,
     OnigOptionType option,
-    OnigEncoding enc,
+    OnigEncodingType* enc,
     OnigSyntaxType* syntax,
     OnigErrorInfo* err_info
 );
 
-void onig_free(OnigRegex);
+void onig_free(regex_t*);
 
 int onig_match(
     regex_t* reg,
