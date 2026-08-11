1ea8975c068d7899003510e4c4fa3ed8b1e01b35
anthony sottile <asottile@umich.edu>
2026-08-11 19:22:56 -0400

    use bdist_wheel from setuptools

--- setup.py
+++ setup.py
@@ -5,22 +5,18 @@ import sys
 import sysconfig
 
 from setuptools import setup
+from setuptools.command.bdist_wheel import bdist_wheel
 
 if (
         platform.python_implementation() == 'CPython' and
         sysconfig.get_config_var('Py_GIL_DISABLED') != 1
 ):
-    try:
-        import wheel.bdist_wheel
-    except ImportError:
-        cmdclass = {}
-    else:
-        class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
-            def finalize_options(self) -> None:
-                self.py_limited_api = f'cp3{sys.version_info[1]}'
-                super().finalize_options()
+    class _bdist_wheel(bdist_wheel):
+        def finalize_options(self) -> None:
+            self.py_limited_api = f'cp3{sys.version_info[1]}'
+            super().finalize_options()
 
-        cmdclass = {'bdist_wheel': bdist_wheel}
+    cmdclass = {'bdist_wheel': _bdist_wheel}
 else:
     cmdclass = {}
 
